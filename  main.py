c4a459ce8899da35568dcbbb81a27cc5198fcb04
Cailus <gabrieldabbah@protonmail.com>
2023-03-19 17:37:46 -0400

    Better options

---  main.py
+++  main.py
@@ -18,6 +18,10 @@ def left_click(delay=40):
     time.sleep(delay / 1000 * random_multiplier())
 
 
+def move_mouse_up(pixels):
+    pyautogui.move(0, -pixels)
+
+
 def scroll_mouse(amount):
     pyautogui.scroll(amount)
 
@@ -29,14 +33,31 @@ def random_multiplier():
 def get_user_input():
     coords = []
     for i in range(2):
-        x = int(input(f"Enter x-coordinate of button {i + 1}:"))
-        y = int(input(f"Enter y-coordinate of button {i + 1}:"))
+        x = int(input(f"Enter x-coordinate of button {i + 1}: "))
+        y = int(input(f"Enter y-coordinate of button {i + 1}: "))
         coords.append((x, y))
 
     # Calculate the second coordinate based on the first
     coords.insert(1, (coords[0][0] - 100, coords[0][1]))
 
-    iterations = int(input("Enter the number of iterations:\n"))
+    # Get user input for the third and fourth coordinates
+    input_third_coord = input("Would you like to input the third coordinate? (y/n): ")
+    if input_third_coord.lower() == "y":
+        x = int(input("Enter x-coordinate of button 3: "))
+        y = int(input("Enter y-coordinate of button 3: "))
+        coords.append((x, y))
+    else:
+        coords.append(None)
+
+    input_fourth_coord = input("Would you like to input the fourth coordinate? (y/n): ")
+    if input_fourth_coord.lower() == "y":
+        x = int(input("Enter x-coordinate of button 4: "))
+        y = int(input("Enter y-coordinate of button 4: "))
+        coords.append((x, y))
+    else:
+        coords.append(None)
+
+    iterations = int(input("Enter the number of iterations: "))
 
     return coords, iterations
 
@@ -79,17 +100,19 @@ def main():
             left_click()
 
             # Third button
-            move_mouse(*click_coords[2])
-            current_color_3 = get_mouse_color()
-            if is_color_close(current_color_3, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_3, (163, 193, 241, 255), 10):
-                left_click()
+            if click_coords[2] is not None:
+                move_mouse(*click_coords[2])
+                current_color_3 = get_mouse_color()
+                if is_color_close(current_color_3, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_3, (163, 193, 241, 255), 10):
+                    left_click()
 
             # Fourth button
-            move_mouse(click_coords[2][0], click_coords[2][1] - 50)
-            current_color_4 = get_mouse_color()
-            if is_color_close(current_color_4, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_4,
-                                                                                            (163, 193, 241, 255), 10):
-                left_click()
+            if click_coords[3] is not None:
+                move_mouse(click_coords[3][0], click_coords[3][1])
+                current_color_4 = get_mouse_color()
+                if is_color_close(current_color_4, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_4,
+                                                                                                (163, 193, 241, 255), 10):
+                    left_click()
 
     except Exception as e:
         print(f"An error occurred: {e}")
---
+++ scroll.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time
+while True:
+
+    pyautogui.scroll(-50)
+    time.sleep(0.1)
