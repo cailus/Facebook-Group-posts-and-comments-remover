c2ffcaf32ee65eebda2a1dd4ee14e1fb9bb9dd4b
Cailus <gabrieldabbah@protonmail.com>
2023-03-18 22:24:10 -0400

    Added and fixed scroll, removed 2nd coordinate

---  main.py
+++  main.py
@@ -1,37 +1,38 @@
 import pyautogui
 import random
 import time
-import tkinter as tk
-from tkinter import simpledialog
 from PIL import ImageGrab
 
-TARGET_COLOR_2 = (56, 56, 57, 255)
+TARGET_COLOR_1 = (56, 56, 57, 255)
 TARGET_COLOR_3 = (55, 120, 224, 255)
-TOLERANCE = 10
+TOLERANCE = 5
+MAX_ITERATIONS = 20
 
 def move_mouse(x, y, delay=100):
     pyautogui.moveTo(x, y, duration=delay / 1000 * random_multiplier())
 
-def left_click(delay=200):
+def left_click(delay=140):  # Reduced sleep amount by 30%
     pyautogui.click()
     time.sleep(delay / 1000 * random_multiplier())
+
+def scroll_mouse(amount):
+    pyautogui.scroll(amount)
 
 def random_multiplier():
     return random.uniform(1, 1.33)
 
 def get_user_input():
-    root = tk.Tk()
-    root.withdraw()
-
-    message = "Run the checkCoordinate.py to see coordinates."
 
     coords = []
-    for i in range(3):
-        x = simpledialog.askinteger("Input", f"Enter x-coordinate of button {i + 1}:\n{message}")
-        y = simpledialog.askinteger("Input", f"Enter y-coordinate of button {i + 1}:\n{message}")
+    for i in range(2):
+        x = int(input(f"Enter x-coordinate of button {i + 1}:"))
+        y = int(input(f"Enter y-coordinate of button {i + 1}:"))
         coords.append((x, y))
 
-    iterations = simpledialog.askinteger("Input", "Enter the number of iterations:")
+    # Calculate the second coordinate based on the first
+    coords.insert(1, (coords[0][0] - 100, coords[0][1] + 6))
+
+    iterations = int(input("Enter the number of iterations:\n"))
 
     return coords, iterations
 
@@ -44,24 +45,39 @@ def get_mouse_color():
     return screen.getpixel((x, y))
 
 def main():
-    click_coords, iterations = get_user_input()
+    try:
+        click_coords, iterations = get_user_input()
 
-    for _ in range(iterations):
-        # First button
-        move_mouse(*click_coords[0])
-        left_click()
-
-        # Second button
-        move_mouse(*click_coords[1])
-        current_color_2 = get_mouse_color()
-        if is_color_close(current_color_2, TARGET_COLOR_2, TOLERANCE):
+        for _ in range(iterations):
+            # First button
+            move_mouse(*click_coords[0])
+            current_color_1 = get_mouse_color()
+            counter = 0
+            while not is_color_close(current_color_1, TARGET_COLOR_1, TOLERANCE):
+                if counter >= MAX_ITERATIONS:
+                    print("Reached maximum scroll iterations, exiting.")
+                    exit()
+                scroll_mouse(-1)
+                time.sleep(0.17)
+                current_color_1 = get_mouse_color()
+                if is_color_close(current_color_1, (104, 104, 106, 255), TOLERANCE):
+                    break
+                print(current_color_1)
+                counter += 1
             left_click()
 
-        # Third button
-        move_mouse(*click_coords[2])
-        current_color_3 = get_mouse_color()
-        if is_color_close(current_color_3, TARGET_COLOR_3, TOLERANCE):
+            # Second button
+            move_mouse(*click_coords[1])
             left_click()
+
+            # Third button
+            move_mouse(*click_coords[2])
+            current_color_3 = get_mouse_color()
+            if is_color_close(current_color_3, TARGET_COLOR_3, TOLERANCE):
+                left_click()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     main()
