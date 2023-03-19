5fa1ce82c149fbb1492dbc561f5e36f1d587a2d2
Cailus <gabrieldabbah@protonmail.com>
2023-03-19 11:53:59 -0400

    Working

---  main.py
+++  main.py
@@ -8,21 +8,25 @@ TARGET_COLOR_3 = (55, 120, 224, 255)
 TOLERANCE = 5
 MAX_ITERATIONS = 20
 
+
 def move_mouse(x, y, delay=100):
     pyautogui.moveTo(x, y, duration=delay / 1000 * random_multiplier())
 
-def left_click(delay=140):  # Reduced sleep amount by 30%
+
+def left_click(delay=140):
     pyautogui.click()
     time.sleep(delay / 1000 * random_multiplier())
+
 
 def scroll_mouse(amount):
     pyautogui.scroll(amount)
 
+
 def random_multiplier():
     return random.uniform(1, 1.33)
 
-def get_user_input():
 
+def get_user_input():
     coords = []
     for i in range(2):
         x = int(input(f"Enter x-coordinate of button {i + 1}:"))
@@ -36,13 +40,16 @@ def get_user_input():
 
     return coords, iterations
 
+
 def is_color_close(color1, color2, tolerance):
     return all(abs(c1 - c2) <= tolerance for c1, c2 in zip(color1, color2))
+
 
 def get_mouse_color():
     x, y = pyautogui.position()
     screen = ImageGrab.grab()
     return screen.getpixel((x, y))
+
 
 def main():
     try:
@@ -53,14 +60,15 @@ def main():
             move_mouse(*click_coords[0])
             current_color_1 = get_mouse_color()
             counter = 0
-            while not is_color_close(current_color_1, TARGET_COLOR_1, 0):
+            while not is_color_close(current_color_1, TARGET_COLOR_1, TOLERANCE):
                 if counter >= MAX_ITERATIONS:
                     print("Reached maximum scroll iterations, exiting.")
                     exit()
                 scroll_mouse(-1)
                 time.sleep(0.17)
                 current_color_1 = get_mouse_color()
-                if is_color_close(current_color_1, (104, 104, 106, 255), 5) or is_color_close(current_color_1, (227, 228, 232, 255), 50):
+                if is_color_close(current_color_1, (104, 104, 106, 255), TOLERANCE) or is_color_close(current_color_1, (
+                227, 228, 232, 255), 50):
                     break
                 print(current_color_1)
                 counter += 1
@@ -76,9 +84,16 @@ def main():
             if is_color_close(current_color_3, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_3, (163, 193, 241, 255), 10):
                 left_click()
 
+            # Fourth button
+            move_mouse(click_coords[2][0], click_coords[2][1] - 50)
+            current_color_4 = get_mouse_color()
+            if is_color_close(current_color_4, TARGET_COLOR_3, TOLERANCE) or is_color_close(current_color_4,
+                                                                                            (163, 193, 241, 255), 10):
+                left_click()
+
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
 if __name__ == '__main__':
     main()
-    
